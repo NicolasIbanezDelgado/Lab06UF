2ff24a379477ba4c0cde36f5b60361e0be76f458
NicolasIbanezDelgado <127369596+NicolasIbanezDelgado@users.noreply.github.com>
2023-03-22 22:43:52 -0400

    Update encode.py

--- encode.py
+++ encode.py
@@ -1,14 +1,24 @@
 #Nicolas Ibanez Delgado
+#This is my own encoder function that I made
 from decode import *
 loop = True 
 
 def encode(password):
     my_str = ''
+    #empty string
+
     for i in range(len(password)):
+        #for loop that iterates the string
         curr_num = int(password[i])
+        #encodes character
         curr_num += 3
+        if curr_num >= 10:
+            #if statement to correctly decode vaalues > 10
+            curr_num = curr_num - 10
         my_str +=  (str(curr_num))
+        #concatenates the string
     password = my_str
+    #returns the string
     return password
 
 
@@ -20,10 +30,12 @@ while loop == True:
         password = input('Please enter your password to encode: ')
         encode(password)
         print( "Your password has been encoded and stored!")
+        #print(encode(password))
         continue
     elif menu == 2: 
-        print(decode("45678888"))
+        print(decode(encode(password)))
         #print (f'The encoded password is {encode(password)}, and the original password is {password}.')
         continue
     elif menu == 3:
+        loop = False
         break
